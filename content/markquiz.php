b486505ebc997f01cf9ee98cdf0eac534555eb50
Jesh15 <103982075@student.swin.edu.au>
2022-05-04 13:30:58 +1000

    layout of the html for php code

--- content/markquiz.php
+++ content/markquiz.php
@@ -1,1 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- import Bootstrap CSS -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+        
+        <!-- site-wide stylesheet -->
+        <link rel = "stylesheet" type = "text/css" href = "styles/style.css">
+        
+        <!-- metadata -->
+        <title>Cookies and Sessions Quiz</title>
+        <meta charset="UTF-8">
+        <meta name="description" content="How to navigate this web page">
+        <meta name="keywords" content="cookies, sessions, education, swinburne, home, menu">
+        <meta name="author" content="Aidan Davies, Jesh Kumar, Dylan Lewis, Nimash Rathnayake, Kasun Jayawardhana">
+    </head>
 
+    <body>
+        <?php
+            if ($attempts == true)
+                // redirection to new page with things shown
+                    
+        ?>
+    </body>
+
+</html>
